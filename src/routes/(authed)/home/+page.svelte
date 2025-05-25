6b44cc8830ed1e5feccb039050e088c235d0350c
PurelyAnecdotal <58897346+PurelyAnecdotal@users.noreply.github.com>
2025-05-24 22:03:02 -0700

    Add shadcn-svelte

---
+++ src/routes/(authed)/home/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	let { data } = $props();
+</script>
+
+<div>
+	<a href="/instance" class="rounded bg-green-700 p-2 text-white">Open instance</a>
+	<a href="/new" class="rounded bg-green-700 p-2 text-white">Create instance</a>
+</div>
+
+{#each data.containers as container (container.Id)}
+	<div class="m-4">
+		<h2 class="text-xl font-bold">{container.Names}</h2>
+		<p>Image: {container.Image}</p>
+		<p>Status: {container.Status}</p>
+	</div>
+{/each}
+
+{#each data.repos as repo (repo.id)}
+	<div class="m-4">
+		<h2 class="text-xl font-bold">{repo.name}</h2>
+		<p>Description: {repo.description}</p>
+		<p>{repo.private ? 'Private' : 'Public'}</p>
+	</div>
+{/each}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,46 @@
 <script lang="ts">
+	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
+	import GitHub from '@lucide/svelte/icons/github';
+	import CircleUser from '@lucide/svelte/icons/circle-user';
+	import ChevronDown from '@lucide/svelte/icons/chevron-down';
+	import LogOutIcon from '@lucide/svelte/icons/log-out';
+	import { page } from '$app/state';
+	import Button, { buttonVariants } from '$lib/components/ui/button/button.svelte';
+	import { signIn, signOut } from '@auth/sveltekit/client';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 
 	let { children } = $props();
 </script>
+
+<ModeWatcher />
+
+<div class="m-4 flex justify-between">
+	Annex
+	{#if page.data.session}
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger class={buttonVariants({ variant: 'ghost' })}>
+				{#if page.data.session.user?.image}
+					<img src={page.data.session.user.image} alt="User Avatar" class="h-4 w-4 rounded-full" />
+				{:else}
+					<CircleUser />
+				{/if}
+				{page.data.session.user?.name}
+				<ChevronDown />
+			</DropdownMenu.Trigger>
+
+			<DropdownMenu.Content class="w-56">
+				<DropdownMenu.Item onclick={() => signOut()}>
+					<LogOutIcon />Log out
+				</DropdownMenu.Item>
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
+	{:else}
+		<Button onclick={() => signIn('github')}>
+			<GitHub />Log in
+		</Button>
+	{/if}
+</div>
+
 
 {@render children()}
